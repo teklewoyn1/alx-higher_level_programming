53a83463a7d1fccea1524695b636c07fa4e49a9d
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-06-01 02:44:37 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -6,6 +6,6 @@ Created on Wed Jun  1 02:04:02 2022
 """
 num = range(100)
 for x in num:
-    print('%02d'.foramt() % (x), end='')
+    print('%02d' % (x), end=''.format())
     if x != 99:
         print(end=', '.format())
