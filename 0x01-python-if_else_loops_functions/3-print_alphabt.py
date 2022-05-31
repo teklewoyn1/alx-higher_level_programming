76a1a6e378196c92ce25548c27f64d12703422c8
teklewoyn1 <teklewoyn79@gmail.com>
2022-06-01 01:36:20 +0300

    a-z except e and q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Created on Wed Jun  1 00:18:36 2022
+
+@author: Teklewoyn
+"""
+a = range(97, 123)
+b = ''
+for x in a:
+    if x == 101:
+        continue
+    elif x == 113:
+        continue
+    b += chr(x)
+print(b.format(), end="")
