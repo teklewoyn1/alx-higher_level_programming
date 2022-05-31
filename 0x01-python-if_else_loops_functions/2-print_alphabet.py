32b5713214993368ee617d252f28302f429822ae
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-06-01 00:54:32 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,9 +2,11 @@
 """
 Created on Wed Jun  1 00:18:36 2022
 
-@author: Vineyard22
+@author: Teklewoyn
 """
 a = range(97, 123)
-
+b=""
 for x in a:
-    print(chr(x), end="")
+    b += chr(x)
+    
+print(b)
