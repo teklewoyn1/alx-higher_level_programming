4c59ea1bfbc54dafbd79566a2c4d9b6985be11f8
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-06-21 01:07:18 +0300

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-    """
-    A function that prints an integer with
-    "{:d}".format()
-    """
         try:
             if i > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
                 result += (a ** b) / i
         except:
