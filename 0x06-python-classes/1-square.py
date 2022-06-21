87e9d03536fe11c4559952a3c659044b2576e9d0
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-06-22 00:11:55 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
+"""Initializes the data."""
+
+
 class Square:
     """Represents a square.
+    
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
